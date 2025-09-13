313523aa719af2351447430855b664d64aacd668
priyanshi7604 <priyanshi7604@gmail.com>
2025-09-13 17:55:14 +0530

    idk

--- src/Url.js
+++ src/Url.js
@@ -1,4 +1,22 @@
-// const Url = "http://localhost:5500/api";
+// // const Url = "http://localhost:5500/api";
+
+// // let token = localStorage.getItem("token");
+
+// // const headers = {
+// //     "ngrok-skip-browser-warning": true,
+// //     "Content-Type": "application/json;charset=UTF-8",
+// //     "Access-Control-Allow-Origin": "*",
+// //     token: token,
+// // };
+
+// // const config = {
+// //     headers: headers,
+// // };
+// // export { Url, config };
+
+// // src/Url.js (ya jahan bhi file hai)
+
+// const Url = "http://localhost:3000/api";
 
 // let token = localStorage.getItem("token");
 
@@ -6,29 +24,39 @@
 //     "ngrok-skip-browser-warning": true,
 //     "Content-Type": "application/json;charset=UTF-8",
 //     "Access-Control-Allow-Origin": "*",
-//     token: token,
+//     token: token ? token : "",
 // };
 
 // const config = {
 //     headers: headers,
 // };
+
 // export { Url, config };
 
-// src/Url.js (ya jahan bhi file hai)
+// src/Url.js
 
-const Url = "http://localhost:3000/api";
+// Automatically switch between local and production backend
+// const BASE_URL =
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:3000/api" // Local backend during development
+//     : "https://inventory-backend-6fxk.onrender.com/api"; // Live backend on Render
 
+const Url = "https://inventory-backend-6fxk.onrender.com/api";
+// Get token from localStorage
 let token = localStorage.getItem("token");
 
+// Common headers for all API calls
 const headers = {
-    "ngrok-skip-browser-warning": true,
-    "Content-Type": "application/json;charset=UTF-8",
-    "Access-Control-Allow-Origin": "*",
-    token: token ? token : "",
+  "ngrok-skip-browser-warning": true,
+  "Content-Type": "application/json;charset=UTF-8",
+  "Access-Control-Allow-Origin": "*",
+  token: token ? token : "",
 };
 
+// Config object for axios
 const config = {
-    headers: headers,
+  headers: headers,
 };
 
+// export { BASE_URL as Url, config };
 export { Url, config };
